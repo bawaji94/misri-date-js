a5809229561b73b0cb23ce87bfe95a60c65a9045
Shabbir Bawaji <bawaji94@gmail.com>
2024-03-11 22:37:10 +0530

    test: error case scenario when invalid arguments

--- tests/MisriDate.test.js
+++ tests/MisriDate.test.js
@@ -1,5 +1,19 @@
 const MisriDate = require('..');
 
+const data = [
+    { "year": 1445, "month": 0, "date": 1 },
+    { "year": 1445, "month": 13, "date": 1 },
+    { "year": 1445, "month": 1, "date": 0 },
+    { "year": 1445, "month": 1, "date": 31 },
+    { "year": 1445, "month": 12, "date": 31 },
+    { "year": 1445, "month": 12, "date": 0 },
+    { "year": 1449, "month": 12, "date": 30 },
+    { "year": 1449, "month": 12, "date": 0 },
+    { "year": "1449", "month": 12, "date": 0 },
+    { "year": 1449, "month": "12", "date": 0 },
+    { "year": 1449, "month": 12, "date": "0" }
+]
+
 describe('Misri Date', () => {
     beforeEach(() => {
         jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
@@ -21,7 +35,16 @@ describe('Misri Date', () => {
         expect(pehliMoharran1444H.year).toEqual(1441);
     });
 
-    it('should throw an error when all parameters are not passed', () => {
-        expect(() => new MisriDate(1)).toThrow('Pass valid arguments');
+    it.each(data)('should not be created when misri date passed is $year, $month, $date', (params) => {
+        const { year, month, date } = params
+        expect(() => new MisriDate(year, month, date)).toThrow('Pass valid arguments');
+    });
+
+    it('should no be created when only year is given', () => {
+        expect(() => new MisriDate(1345)).toThrow('Pass valid arguments');
+    });
+
+    it('should no be created when only year and month is given', () => {
+        expect(() => new MisriDate(1345, 1)).toThrow('Pass valid arguments');
     });
 });
